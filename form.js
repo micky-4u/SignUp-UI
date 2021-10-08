584ff7037ffa2d1ced982747e4c967993ebfd5b1
Michael Laryea <80675696+micky-4u@users.noreply.github.com>
2021-10-08 10:10:41 +0000

    Create form.js
    Inputs handler

---
+++ form.js
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import React , {useState} from 'react';
+import {  Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import styles from "./styles";
+
+const Form = () =>{
+    const [text, setText] = useState("")
+    return(
+        <View styles ={styles.container}>
+
+            <Text style = {styles.header}>Register</Text>
+            
+            <TextInput 
+                style = {styles.input}
+                onChangeText = {setText}
+                placeholder = "Your name"
+                underlineColorAndroid = {'transparent'}
+            />
+            <TextInput 
+                style = {styles.input}
+                onChangeText = {setText}
+                placeholder = "User Id"
+                underlineColorAndroid = {'transparent'}
+            />
+            <TextInput 
+                style = {styles.input}
+                onChangeText = {setText}
+                placeholder = "Email"
+                underlineColorAndroid = {'transparent'}
+            />
+            <TextInput 
+                style = {styles.input}
+                onChangeText = {setText}
+                placeholder = "Password"
+                underlineColorAndroid = {'transparent'}
+                secureTextEntry = {true}
+            />
+            <TextInput 
+                style = {styles.input}
+                onChangeText = {setText}
+                placeholder = "Confirm Password"
+                underlineColorAndroid = {'transparent'}
+            />
+
+            <Button 
+                onPress = {()=> alert("Thanks for Registering")}
+                title = "Sign Up"
+            />
+        </View>
+
+        
+    )
+}
+
+
+export default Form;
